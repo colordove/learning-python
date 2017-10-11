7aa8e123af4b038a0f5cf017f438aa1a66e5737c
HuJian <624598206@qq.com>
2017-10-11 17:49:30 +0800

    feat(process): Pool

--- process.py
+++ process.py
@@ -9,17 +9,39 @@
 #     print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
 
 # multiprocessing module
-from multiprocessing import Process
-import os
+# from multiprocessing import Process
+# import os
 
-# 子进程要执行的代码
-def run_proc(name):
-    print('Run child process %s (%s)...' % (name, os.getpid()))
+# # 子进程要执行的代码
+# def run_proc(name):
+#     print('Run child process %s (%s)...' % (name, os.getpid()))
+
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Process(target = run_proc, args = ('test', ))
+#     print('Child process will start.')
+#     p.start()
+#     p.join()
+#     print('Child process end.')
+
+# Pool进程池
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task is %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s run %0.2f seconds' % (name, end- start))
 
 if __name__ == '__main__':
     print('Parent process %s.' % os.getpid())
-    p = Process(target = run_proc, args = ('test', ))
-    print('Child process will start.')
-    p.start()
+    # 最多同时执行4个进程
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
     p.join()
-    print('Child process end.')
+    print('All subprocesses done')
