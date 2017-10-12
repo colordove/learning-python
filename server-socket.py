21acfdbabb66d3ac2ad288724ef4d6aee334a3d8
HuJian <624598206@qq.com>
2017-10-12 09:51:49 +0800

    fix(server-socket.py): fix error

--- server-socket.py
+++ server-socket.py
@@ -1,16 +1,10 @@
-import socket, threading
+import socket, threading, time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 9999))
 
 s.listen(5)
 print('wating for connection...')
-
-while True:
-    sock, addr = s.accept
-    # 创建新线程来处理TCP连接
-    t = threading.Thread(target = tcplink, args = (sock, addr))
-    t.start()
 
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
@@ -20,6 +14,12 @@ def tcplink(sock, addr):
         time.sleep(1)
         if not data or data.decode('utf-8') == 'exit':
             break
-        sock.send(('Hello, %s!') % data.decode('utf-8').encode('utf-8'))
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
+
+while True:
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接
+    t = threading.Thread(target = tcplink, args = (sock, addr))
+    t.start()
